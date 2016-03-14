89e1a011ebdbd0cf7b426d6a00e37854d9fbcfd6
Kasey McCormick <kasey.mccormick@atssecured.com>
2016-03-14 17:16:15 -0500

    removed prevent default from login events

--- hello.js
+++ hello.js
@@ -32,7 +32,6 @@ if (Meteor.isClient) {
 
     Template.login.events({
         'submit form': function (event) {
-            event.preventDefault();
             var emailVar = event.target.loginEmail.value;
             var passwordVar = event.target.loginPassword.value;
             console.log("Form submitted.");
