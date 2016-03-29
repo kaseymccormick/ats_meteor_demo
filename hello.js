bd90a1d0950ffb5341c1aa7a91f5161c263d1b86
Kasey McCormick <kasey.mccormick@atssecured.com>
2016-03-29 11:31:15 -0500

    removing helpers not needed

--- hello.js
+++ hello.js
@@ -37,18 +37,7 @@ if (Meteor.isClient) {
             })
         }
     });
-    Template.body.events({
-        'click button.modal': function(event, template) {
-            var name = template.$(event.target).data('modal-template');
-            Session.set('activeModal', name);
-        }
-    });
 
-    Template.modal.helpers({
-        activeModal: function() {
-            return Session.get('activeModal');
-        }
-    });
     Template.login.events({
         'submit form': function (event) {
             event.preventDefault();
